643798d03480fee030aaf9d05ba748314cf4139a
Sinus <Sinus75s@mail.ru>
2022-03-03 14:03:25 +0300

    Add event registration design

---
+++ app/src/main/java/com/example/practic/FragmentRegistrationEvent.java
@@ -0,0 +1,20 @@
+package com.example.practic;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentRegistrationEvent extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View thisView = inflater.inflate(R.layout.fragment_registration_event, container, false);
+        return thisView;
+    }
+}
